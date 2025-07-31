62b793fc98e8cb7acb73357075b7532857affee3
Sawsdevx8 <sawsdevx8@gmail.com>
2025-07-30 20:52:04 -0500

    Feat: add command execution on the cli program

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,10 +1,12 @@
 package cli
 
 import (
-	"fmt"
-	"strings"
-	"os"
 	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sawsdevx8/tasktracker/internal/tasklist"
 )
 
 const (
@@ -39,22 +41,54 @@ func IsValidCommand(command string) bool {
 func GetCommandDescription(command string) string {
 	switch command {
 	case ADD:
-		return "Add a task to the list ej: add 'Make coffee'"
+		return "Add a task to the list \nej: add \"Make coffee\""
 	case DELETE:
-		return "Delete a task from the list ej: delete 1"
+		return "Delete a task from the list \nej: delete 1"
 	case LIST:
 		return "List all tasks"
 	case UPDATE:
-		return "Update a task ej: update 1 'Make coffee with milk'"
+		return "Update a task \nej: update 1 'Make coffee with milk'"
 	case MARKDONE:
-		return "Mark a task as done ej: mark-done 1"
+		return "Mark a task as done \nej: mark-done 1"
 	case MARKPROGRESS:
-		return "Mark a task as in progress"
+		return "Mark a task as in progress \nej: mark-progress 1"
 	case TERMINATE:
 		return "Terminate the program: terminate"
 	default:
 		return fmt.Sprintf("Unknown command: %s", command)
 	}
+}
+
+func GetDescriptionFromInput(input string) string {
+	description := strings.Split(input, "\"")
+	return description[1]
+}
+
+func ExecuteCommand(command string, userInput *string, tasks *tasklist.TaskList) {
+	fmt.Println("Executing command:", command)
+	cleanCommand := strings.TrimSpace(command)
+	switch cleanCommand {
+	case ADD:
+		if !strings.Contains(*userInput, "\"") {
+			fmt.Println("Invalid input, no correct description found")
+			fmt.Println("ej: add \"Make coffee\"")
+			return
+		}
+		tasklist.AddNewTask(tasks, GetDescriptionFromInput(*userInput))
+	case DELETE:
+		// tasklist.DeleteTask(&tasklist.TaskList, command[2])
+	case LIST:
+		tasklist.ShowTaskList(tasks)
+	case UPDATE:
+		// tasklist.UpdateTask(&tasklist.TaskList, command[2], command[3])
+	case MARKDONE:
+		// tasklist.MarkTaskAsDone(&tasklist.TaskList, command[2])
+	case MARKPROGRESS:
+	// tasklist.MarkTaskAsInProgress(&tasklist.TaskList, command[2])
+	default:
+		fmt.Println("Unknown command:", command[1])
+	}
+
 }
 
 func IsValidCommandInput(input string) bool {
@@ -76,42 +110,41 @@ func IsValidCommandInput(input string) bool {
 	return true
 }
 
-func GetCommand(input string) string {
-	inputParts := strings.Split(input, " ")
-	return inputParts[1]
-}
 
-func GetUserInput(completeText *string) {
+func GetUserInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	*completeText += text
+	return text
 }
 
 func Start() {
+
+	tasks := tasklist.NewTaskList()
 	fmt.Println("Task CLI")
 	fmt.Println("Available commands:")
 	for command, description := range cliCommands {
-		fmt.Printf("%s: %s\n", command, description)
+		fmt.Println(command, ":")
+		fmt.Println(GetCommandDescription(description))
+		fmt.Println("----------------")
 	}
 	fmt.Println("Type the instruction preceded by 'task-cli'")
 	fmt.Println("Type 'terminate' to exit")
 	userInput := ""
 	for {
-		GetUserInput(&userInput)
-		
+		userInput = GetUserInput()
+
 		if !IsValidCommandInput(userInput) {
 			fmt.Println("Invalid input")
 			continue
 		}
-		command := strings.TrimSpace(GetCommand(userInput))
-		fmt.Println(GetCommandDescription(command))
+		userCommand := strings.Split(userInput, " ")
+		command := strings.TrimSpace(userCommand[1])
+		ExecuteCommand(command, &userInput, &tasks)
 		userInput = ""
 		if command == "terminate" {
 			fmt.Println("Goodbye")
 			break
 		}
 	}
-	
+
 }
-
-
