2b5ae6979bb76f94e66b527af6bc03932fb16b4d
Sawsdevx8 <sawsdevx8@gmail.com>
2025-08-11 11:52:38 -0500

    Feat: Add file management packacge and functions

---
+++ internal/file/file.go
@@ -0,0 +1,40 @@
+package file
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var baseRoute string = "assets/" 
+
+func CreateFile(filename string){
+
+	file, err := os.Create(baseRoute + filename)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("File created at", baseRoute + filename)
+}
+
+func FileExists(filename string) bool{
+
+	if file, err := os.Stat(baseRoute + filename); err == nil{
+		fmt.Println(file.Name())
+		return true
+	}
+
+	return false
+}
+
+func ReadFile(filename string) ([]byte, error){
+	file, err := os.ReadFile(baseRoute + filename)
+	if err != nil {
+		log.Fatal(err)
+		return file, err
+	}
+	return file, err
+
+}
